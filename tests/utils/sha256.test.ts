cc59118aedb3befe030ddcbc1869473da0aac757
Vsevolod Girenko <vs.girenko@mail.ru>
2023-12-19 19:38:07 +0300

    dev: using hash constant in test

--- tests/utils/sha256.test.ts
+++ tests/utils/sha256.test.ts
@@ -1,4 +1,5 @@
 import { sha256 } from '../../src/utils/sha256'
+import { exampleHash } from '../helpers/exampleHash'
 
 describe('sha256 hash util', () => {
   it('produces 64 characters hex digest', async () => {
@@ -9,7 +10,7 @@ describe('sha256 hash util', () => {
   })
 
   it('produces correct hash', async () => {
-    const expected: string = '388c735eec8225c4ad7a507944dd0a975296baea383198aa87177f29af2c6f69'
+    const expected: string = exampleHash
     const hash = await sha256('test@example.org')
 
     expect(hash).toStrictEqual(expected)
